2ceac8a081dc9a048c26ac3ffb22ae21150c7647
286179-EugeneRussel <eugene.russel@usthealthproof.com>
2025-03-03 13:33:37 +0530

    redirect component

--- claims/src/app/app.routes.ts
+++ claims/src/app/app.routes.ts
@@ -4,13 +4,17 @@ import { PendingClaimsAdjustedComponent } from './pending-claims-adjusted/pendin
 import { PendingClaimsNewComponent } from './pending-claims-new/pending-claims-new.component';
 import { FinalizedClaimsComponent } from './finalized-claims/finalized-claims.component';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { RedirectComponent } from './guard/RedirectComponent';
+import { AuthGuard } from './guard/auth-guard';
 
 export const routes: Routes = [
-    { path: 'productionReport', component: ProductionReportComponent },
-    { path: 'pendingClaimsNew', component: PendingClaimsNewComponent },
-    { path: 'pendingClaimsAdjusted', component: PendingClaimsAdjustedComponent },
-    { path: 'finalizedClaims', component: FinalizedClaimsComponent },
+    { path: 'productionReport', component: ProductionReportComponent,canActivate:[AuthGuard] },
+    { path: 'pendingClaimsNew', component: PendingClaimsNewComponent,canActivate:[AuthGuard] },
+    { path: 'pendingClaimsAdjusted', component: PendingClaimsAdjustedComponent,canActivate:[AuthGuard] },
+    { path: 'finalizedClaims', component: FinalizedClaimsComponent,canActivate:[AuthGuard] },
     { path: '', redirectTo:'productionReport',
         pathMatch:'full'
-    }
+    },
+    {path:'login', component:RedirectComponent}
 ];
